5b9412b49621995ffd8b99f12a6590b49413778a
AustinXY <ayxue@ucdavis.edu>
2019-06-01 13:38:21 -0700

    key binding done; first minigame done;

---
+++ Deep Space Delivery/Assets/Scripts/P2Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Controller : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
