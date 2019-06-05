1582bd1954f4b39813f2f3efa9fdb48f9d2b2315
glaive345 <dereklin.usa@gmail.com>
2019-06-05 00:31:18 -0700

    Scripts and animations

---
+++ Deep Space Delivery/Assets/Scripts/UI_Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Timer : MonoBehaviour
+{
+    [SerializeField] private Text UIText;
+    [SerializeField] private float StartingTime;
+
+    private float TimePassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimePassed = 0.0f + StartingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimePassed += Time.deltaTime;
+        UIText.text = "Time: " + TimePassed.ToString("F");
+    }
+}
