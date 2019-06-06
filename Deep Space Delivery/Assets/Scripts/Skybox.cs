b090a1e584847b17144d82002b953ff8557c4b57
glaive345 <dereklin.usa@gmail.com>
2019-06-06 14:07:21 -0700

    Skybox rotation

---
+++ Deep Space Delivery/Assets/Scripts/Skybox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skybox : MonoBehaviour
+{
+    [SerializeField] private float RotationSpeed;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationSpeed);
+    }
+
+
+}
