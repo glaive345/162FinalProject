a3822eeec1cef4c4470ba3e7ab3a2a57c79ef020
glaive345 <dereklin.usa@gmail.com>
2019-06-10 22:19:00 -0700

    Radar Game

---
+++ Deep Space Delivery/Assets/Scripts/Interactable Objects/RadarZone.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarZone : MonoBehaviour
+{
+    [SerializeField] private GameObject displayPanel;
+
+    private bool gameActivated;
+    private string currentPlayer;
+
+    //ADD OTHER VARIABLES HERE
+
+
+
+
+    void Start()
+    {
+        this.gameActivated = false;
+        this.currentPlayer = "None";
+
+        //PREINITIALIZE VARIABLES HERE
+
+
+
+        this.displayPanel.SetActive(false);
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        //If player1 or player 2 interacts
+        if ((other.gameObject.name == "Player1" && Input.GetButtonDown("Utility1")) || (other.gameObject.name == "Player2" && Input.GetButtonDown("Utility2")))
+        {
+            if (gameActivated == false)
+            {
+                //Initializing Game
+                //INITIALIZE START STATE HERE
+
+
+                this.displayPanel.SetActive(true);
+
+                //Setting Starting Player
+                this.gameActivated = true;
+                this.currentPlayer = other.gameObject.name;
+
+                //Positions panel in correct location
+                if (currentPlayer == "Player2")
+                {
+                    this.displayPanel.transform.localPosition = new Vector3(734.5284f, 0, 0);
+                }
+                else
+                {
+                    this.displayPanel.transform.localPosition = new Vector3(-734.5284f, 0, 0);
+                }
+            }
+            //Progresses game if player who initiated it interacts
+            else if (currentPlayer == other.gameObject.name)
+            {
+                //Playing Game
+                //ADD ON-INTERACT EFFECT HERE
+
+
+
+                //NEED TO SEND RESULTS TO SUBSCRIBERS
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //If activating player leaves after starting
+        if (other.gameObject.name == currentPlayer && gameActivated)
+        {
+            //Closing Game
+            //RESET VARIABLES HERE
+
+
+
+            this.gameActivated = false;
+            Debug.Log("game deactivated");
+
+            this.displayPanel.SetActive(false);
+
+            currentPlayer = "None";
+
+
+            //NEED TO SEND EARLY EXIT TO SUBSCRIBERS
+        }
+    }
+}
