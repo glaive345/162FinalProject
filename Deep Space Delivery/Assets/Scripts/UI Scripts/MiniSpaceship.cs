8edb236e13f9dcbfd2dc83c69c6c2af7dfee8c10
agdrall <Aaron Kim>
2019-06-11 00:02:04 -0700

    Changed Main Menu

---
+++ Deep Space Delivery/Assets/Scripts/UI Scripts/MiniSpaceship.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniSpaceship : MonoBehaviour
+{
+    void Start()
+    {
+        this.gameObject.transform.localPosition = new Vector3(-300, Random.Range(-150.0f, 150.0f), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.transform.localPosition = new Vector3(this.gameObject.transform.localPosition.x + 0.5f, this.gameObject.transform.localPosition.y, 0);
+    }
+}
