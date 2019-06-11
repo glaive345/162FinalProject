69ab22e5f4bb2f6c367eefe4da97b2c64a8ff481
glaive345 <dereklin.usa@gmail.com>
2019-06-10 23:42:47 -0700

    Radar Game WIP

--- Deep Space Delivery/Assets/Scripts/Interactable Objects/BridgeZone.cs
+++ Deep Space Delivery/Assets/Scripts/Interactable Objects/BridgeZone.cs
@@ -8,8 +8,9 @@ public class BridgeZone : MonoBehaviour
     [SerializeField] private GameObject displayPanel;
     private List<GameObject> warningWindows = new List<GameObject>();
 
-    [SerializeField] private GameObject congratulations;
     [SerializeField] private GameObject warningWindowPrefab;
+
+    [SerializeField] private float throttleReset;
 
     private bool gameActivated;
     private string currentPlayer;
@@ -27,21 +28,9 @@ public class BridgeZone : MonoBehaviour
         this.gameActivated = false;
         this.currentPlayer = "None";
         this.systemHealth = 100;
-        this.throttle = Random.Range(0.0f, 10.0f);
+        this.throttle = throttleReset;
         this.remainingWindows = 0;
 
-        // for(int i = 0; i < 20; i++)
-        // {
-        //     this.warningWindows.Add(this.displayPanel.transform.GetChild(i).gameObject);
-        // }
-
-        // this.displayPanel.SetActive(false);
-        // this.warningWindows.ForEach(delegate (GameObject obj)
-        // {
-        //     obj.SetActive(false);
-        // });
-
-        // this.congratulations.SetActive(false);
     }
 
     void Update()
@@ -59,18 +48,13 @@ public class BridgeZone : MonoBehaviour
                 }
             }
             this.systemHealth -= randDamage;
-            this.throttle = Random.Range(0.0f, 10.0f);
+            this.throttle = throttleReset;
         }
-        // if (this.systemHealth != 100)
-        // {
-        //     Debug.Log(this.systemHealth);
-        // }
     }
 
     private void createWindow()
     {
-        GameObject tempWindow = Instantiate(
-            warningWindowPrefab, this.displayPanel.transform.position, this.displayPanel.transform.rotation);
+        GameObject tempWindow = Instantiate(warningWindowPrefab, this.displayPanel.transform.position, this.displayPanel.transform.rotation);
         tempWindow.GetComponent<RectTransform>().sizeDelta = new Vector2(175.34f, 56.91f);
         tempWindow.transform.SetParent(this.displayPanel.transform);
         tempWindow.GetComponent<RectTransform>().localScale = new Vector3(1.0f, 1.0f, 1.0f);
@@ -82,19 +66,13 @@ public class BridgeZone : MonoBehaviour
     private void OnTriggerStay(Collider other)
     {
         //If player1 or player 2 interacts
-        if ((other.gameObject.name == "Player1" && Input.GetButtonDown("Utility1"))
-            || (other.gameObject.name == "Player2" && Input.GetButtonDown("Utility2")))
+        if ((other.gameObject.name == "Player1" && Input.GetButtonDown("Utility1")) || (other.gameObject.name == "Player2" && Input.GetButtonDown("Utility2")))
         {
             if (!this.gameActivated)
             {
                 // activating Game
                 this.displayPanel.SetActive(true);
-                var temp = 100 - this.systemHealth - this.remainingWindows;
-                for (var i = 0; i < temp; i++)
-                {
-                    this.createWindow();
-                    this.remainingWindows++;
-                }
+
 
                 //Setting Starting Player
                 this.gameActivated = true;
@@ -110,7 +88,7 @@ public class BridgeZone : MonoBehaviour
                     this.displayPanel.transform.localPosition = new Vector3(-734.5284f, 0, 0);
                 }
             }
-            else if (this.systemHealth < 100)
+            else if (currentPlayer == other.gameObject.name && this.systemHealth < 100)
             {
                 var lastChild = this.displayPanel.transform.GetChild(this.displayPanel.transform.childCount - 1);
                 // Debug.Log(lastChild);
@@ -119,25 +97,6 @@ public class BridgeZone : MonoBehaviour
                 this.systemHealth++;
                 this.remainingWindows--;
             }
-
-            //Progresses game if player who initiated it interacts
-            // else if(currentPlayer == other.gameObject.name)
-            // {
-            //     //Playing Game
-            //     if(remainingWindows > 0)
-            //     {
-            //         //Removes one window
-            //         this.remainingWindows--;
-            //         warningWindows[this.remainingWindows].SetActive(false);
-            //     }
-            //     else
-            //     {
-            //         //Game Completed
-            //         this.congratulations.SetActive(true);
-
-            //         //NEED TO SEND RESULTS TO SUBSCRIBERS
-            //     }
-            // }
         }
     }
 
@@ -151,16 +110,9 @@ public class BridgeZone : MonoBehaviour
             Debug.Log("game deactivated");
 
             this.displayPanel.SetActive(false);
-            // this.warningWindows.ForEach(delegate (GameObject obj)
-            // {
-            //     obj.SetActive(false);
-            // });
-            // this.remainingWindows = 0;
+
             currentPlayer = "None";
-
-            this.congratulations.SetActive(false);
-
-            //NEED TO SEND EARLY EXIT TO SUBSCRIBERS
+            
         }
     }
 }
--- Deep Space Delivery/Assets/Scripts/Interactable Objects/RadarZone.cs
+++ Deep Space Delivery/Assets/Scripts/Interactable Objects/RadarZone.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadarZone : MonoBehaviour
 {
@@ -10,8 +11,22 @@ public class RadarZone : MonoBehaviour
     private string currentPlayer;
 
     //ADD OTHER VARIABLES HERE
+    [SerializeField] private GameObject scanPrefab;
+    private Text scansRequiredText;
+    private Text nextScanText;
+    private Text scansLeftText;
+    private Text numberText;
+    private GameObject check;
+    private GameObject cross;
+    private GameObject nextScanBar;
+    private GameObject scanTimeBar;
+    private GameObject scanArea;
+    private Material checkMaterial;
+    private Material crossMaterial;
 
-
+    [SerializeField] private float nextScanTime;
+    [SerializeField] private float currentScanTime;
+    private float currentTimer;
 
 
     void Start()
@@ -20,15 +35,42 @@ public class RadarZone : MonoBehaviour
         this.currentPlayer = "None";
 
         //PREINITIALIZE VARIABLES HERE
+        scansRequiredText = displayPanel.transform.GetChild(0).GetComponent<Text>();
+        nextScanText = displayPanel.transform.GetChild(4).GetComponent<Text>();
+        scansLeftText = displayPanel.transform.GetChild(5).GetComponent<Text>();
+        numberText = displayPanel.transform.GetChild(8).GetComponent<Text>();
+        check = displayPanel.transform.GetChild(10).gameObject;
+        cross = displayPanel.transform.GetChild(9).gameObject;
+        nextScanBar = displayPanel.transform.GetChild(3).gameObject;
+        scanTimeBar = displayPanel.transform.GetChild(7).gameObject;
+        scanArea = displayPanel.transform.GetChild(1).gameObject;
+        checkMaterial = check.GetComponent<Renderer>().material;
+        crossMaterial = cross.GetComponent<Renderer>().material;
 
-
+        currentTimer = 0;
 
         this.displayPanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
     }
 
 
     private void OnTriggerStay(Collider other)
     {
+        if (gameActivated)
+        {
+            currentTimer += Time.deltaTime;
+            if(currentTimer > currentScanTime)
+            {
+                //Checks if number matches
+
+                currentTimer = 0;
+            }
+        }
+
         //If player1 or player 2 interacts
         if ((other.gameObject.name == "Player1" && Input.GetButtonDown("Utility1")) || (other.gameObject.name == "Player2" && Input.GetButtonDown("Utility2")))
         {
@@ -36,7 +78,9 @@ public class RadarZone : MonoBehaviour
             {
                 //Initializing Game
                 //INITIALIZE START STATE HERE
-
+                currentTimer = 0;
+                ChangeMaterialMode(checkMaterial, BlendMode.Fade);
+                ChangeMaterialMode(crossMaterial, BlendMode.Fade);
 
                 this.displayPanel.SetActive(true);
 
@@ -86,6 +130,46 @@ public class RadarZone : MonoBehaviour
 
 
             //NEED TO SEND EARLY EXIT TO SUBSCRIBERS
+        }
+    }
+
+    private void createScanObject()
+    {
+        GameObject tempWindow = Instantiate(scanPrefab, this.displayPanel.transform.position, this.displayPanel.transform.rotation);
+        tempWindow.transform.SetParent(this.displayPanel.transform);
+        tempWindow.GetComponent<RectTransform>().localScale = new Vector3(5.0f, 5.0f, 5.0f);
+        tempWindow.transform.localPosition = new Vector3(Random.Range(-125, 125f), Random.Range(-65f, 65f), 0);
+    }
+
+    //Code to change material mode (Adapted from: https://answers.unity.com/questions/1004666/change-material-rendering-mode-in-runtime.html)
+    private enum BlendMode
+    {
+        Opaque,
+        Fade,
+    }
+
+    private void ChangeMaterialMode(Material standardShaderMaterial, BlendMode blendMode)
+    {
+        switch (blendMode)
+        {
+            case BlendMode.Opaque:
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                standardShaderMaterial.SetInt("_ZWrite", 1);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = -1;
+                break;
+            case BlendMode.Fade:
+                standardShaderMaterial.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                standardShaderMaterial.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                standardShaderMaterial.SetInt("_ZWrite", 0);
+                standardShaderMaterial.DisableKeyword("_ALPHATEST_ON");
+                standardShaderMaterial.EnableKeyword("_ALPHABLEND_ON");
+                standardShaderMaterial.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                standardShaderMaterial.renderQueue = 3000;
+                break;
         }
     }
 }
